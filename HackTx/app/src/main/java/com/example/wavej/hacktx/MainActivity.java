d04d176cb35d2bf785933e79ffe83423ba50795e
Jack Baumann <wavejbaumann@gmail.com>
2015-09-26 16:41:51 -0500

    Added Date and Time Fragments
    Added a DialogFragment for Date and That opens on a button click.

--- HackTx/app/src/main/java/com/example/wavej/hacktx/MainActivity.java
+++ HackTx/app/src/main/java/com/example/wavej/hacktx/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.wavej.hacktx;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +42,71 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Manages the TimePicker dialog
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            setProfileTime(hourOfDay, minute);
+        }
+    }
+
+    //Manages the DatePicker dialog
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            setProfileDate(year, month, day);
+        }
+    }
+
+    //Shows the search dialog when the search button is clicked
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    //Shows the search dialog when the search button is clicked
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    //Sets a profile at the given time
+    private static void setProfileTime(int hour, int minute){
+        //TODO
+    }
+
+    //Sets a profile at the given date
+    private static void setProfileDate(int year, int month, int day){
+        //TODO
     }
 }
