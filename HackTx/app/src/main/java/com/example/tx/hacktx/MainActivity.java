bd73cdd5f041e7982fac5731ec3005587f7eca3c
Jack Baumann <wavejbaumann@gmail.com>
2015-09-27 09:10:19 -0500

    Added SharedPreferences

--- HackTx/app/src/main/java/com/example/tx/hacktx/MainActivity.java
+++ HackTx/app/src/main/java/com/example/tx/hacktx/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+
+import java.io.File;
 import java.util.Calendar;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -29,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private AudioManager am;
 
     public static ArrayList<Profile> profileList = new ArrayList<>();
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+
+    private static boolean newProfile = false;
 
     private PendingIntent pendingIntent;
     
@@ -56,8 +63,15 @@ public class MainActivity extends AppCompatActivity {
             alarm = new Alarm();
             alarm.checkTime();
         }
+
+        getProfilesFromStorage();
     }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+        storeProfilesToStorage();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,6 +96,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getProfilesFromStorage(){
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        boolean[] repeats = new boolean[7];
+        repeats[0] = settings.getBoolean("repeatM", true);
+        repeats[1] = settings.getBoolean("repeatT", true);
+        repeats[2] = settings.getBoolean("repeatW", true);
+        repeats[3] = settings.getBoolean("repeatH", true);
+        repeats[4] = settings.getBoolean("repeatF", true);
+        repeats[5] = settings.getBoolean("repeatS", true);
+        repeats[6] = settings.getBoolean("repeatU", true);
+        Profile p = new Profile(settings.getString("name", null), settings.getString("description", null), settings.getInt("ringerState", 0), settings.getInt("hour", 0),
+                settings.getInt("minute", 0), settings.getInt("year", 0), settings.getInt("month", 0), settings.getInt("day", 0), repeats);
+        Log.d("refresh", p.getName());
+        profileList.add(p);
+    }
+
+    public void clearStoredProfiles(){
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    public void storeProfilesToStorage(){
+        clearStoredProfiles();
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+
+        for(Profile p: profileList) {
+            editor.putString("name", p.getName());
+            editor.putString("description", p.getDescription());
+            editor.putInt("ringerState", p.getRingerState());
+            editor.putInt("hour", p.getStartHour());
+            editor.putInt("minute", p.getStartMinute());
+            editor.putInt("year", p.getYear());
+            editor.putInt("month", p.getMonth());
+            editor.putInt("day", p.getDay());
+            editor.putBoolean("repeatM", p.getRepeatDays()[0]);
+            editor.putBoolean("repeatT", p.getRepeatDays()[1]);
+            editor.putBoolean("repeatW", p.getRepeatDays()[2]);
+            editor.putBoolean("repeatH", p.getRepeatDays()[3]);
+            editor.putBoolean("repeatF", p.getRepeatDays()[4]);
+            editor.putBoolean("repeatS", p.getRepeatDays()[5]);
+            editor.putBoolean("repeatU", p.getRepeatDays()[6]);
+        }
+
+        // Commit the edits!
+        editor.commit();
     }
 
     public void buttonClick(View v) {
@@ -130,18 +199,20 @@ public class MainActivity extends AppCompatActivity {
                             {
                                 Profile p = profileList.get(i);
                                 Calendar c = Calendar.getInstance();
-                                c.set( p.getYear(), p.getMonth(), p.getDay(), p.getStartHour(), p.getStartMinute());
+                                c.set(p.getYear(), p.getMonth(), p.getDay(), p.getStartHour(), p.getStartMinute());
                                 long mils = c.getTimeInMillis();
-                                if(System.currentTimeMillis() + 1000 > mils)
-                                {
+                                if(System.currentTimeMillis() + 1000 > mils) {
                                     changeState(p.getRingerState());
                                     profileList.remove(i);
                                     i--;
                                     eventAdapter.update();
                                     eventAdapter.notifyDataSetChanged();
                                     triggerNotification("Volume profile changed");
-
                                 }
+                            }
+                            if(newProfile){
+                                storeProfilesToStorage();
+                                newProfile = false;
                             }
                         }
                     }, 0, 10, TimeUnit.SECONDS);
@@ -168,5 +239,9 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(0, mBuilder.build());
+    }
+
+    public static void setCreatedNewProfile(){
+        newProfile = true;
     }
 }
